7ab112804fc46c4b21873e7db99e60cfe550de88
Shubham Singh <coolsks94@gmail.com>
2019-03-30 00:04:47 +0100

    rolex: Add support to build both gapps and non-gapps build
      Conflicts:
    	aosp_rolex.mk
    
    Change-Id: Ie977d2fcf9caebe259d8819312fe575b24aa93da

--- aosp_rolex.mk
+++ aosp_rolex.mk
@@ -45,3 +45,12 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="rolex-user 7.1.2 N2G47H V9.2.6.0.NCCMIEK release-keys"
 
 BUILD_FINGERPRINT := Xiaomi/rolex/rolex:7.1.2/N2G47H/V9.2.6.0.NCCMIEK:user/release-keys
+
+# Use Gapps
+# Set this flag in build script
+ifeq ($(CURRENT_BUILD_TYPE), gapps)
+TARGET_SHIPS_SEPERATE_GAPPS_BUILD := true
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+endif
